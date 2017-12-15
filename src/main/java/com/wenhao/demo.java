e3b2eb36967b3fafbdc1fb2d41644ae59959d3d8
Zhaowenhor <wenhora@gmail.com>
2017-12-15 11:42:47 +0800

    第四次提交测试11.42。

--- src/main/java/com/wenhao/demo.java
+++ src/main/java/com/wenhao/demo.java
@@ -1,10 +1,7 @@
 package com.wenhao;
 
-/**
- * Created by lenovo on 2017/12/15.
- */
 public class demo {
     public static void main(String[] args) {
-    
+        int a = 1;
     }
 }
---
+++ src/main/java/com/wenhao/demo2.java
@@ -0,0 +1,11 @@
+package com.wenhao;
+
+/**
+ * @Author: ZhaoWenHao
+ * @Data: 2017/12/15
+ */
+public class demo2 {
+    public static void main(String[] args) {
+        System.out.println("OK！");
+    }
+}
